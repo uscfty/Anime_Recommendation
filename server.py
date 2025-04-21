6b3fa159b4687f8382ec940100fbf944039eb521
Tianyu Fu <128670842+uscfty@users.noreply.github.com>
2025-04-21 18:33:41 -0400

    Update server.py

--- server.py
+++ server.py
@@ -352,5 +352,9 @@ def rate_anime():
     return jsonify({'message': 'Rating received!'})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render 会传入 PORT 环境变量
+    app.run(host="0.0.0.0", port=port, debug=True)
